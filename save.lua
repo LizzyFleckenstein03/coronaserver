97f2905a5a1b31fdc88648950bc0f82e1d721a1b
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-05-04 13:52:22 +0200

    Added birthday, split code into files and fix saving of grantall

---
+++ save.lua
@@ -0,0 +1,15 @@
+function coronaserver.load()
+	local file = io.open(minetest.get_worldpath() .. "/coronaserver", "r")
+	if file then
+		coronaserver.savedata = minetest.deserialize(file:read())
+		file:close()
+	else
+		coronaserver.savedata = {}
+	end
+end
+function coronaserver.save()
+	local file = io.open(minetest.get_worldpath() .. "/coronaserver", "w")
+	file:write(minetest.serialize(coronaserver.savedata))
+	file:close()
+end
+coronaserver.load() 
