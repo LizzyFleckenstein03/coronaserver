54cef1de06002c8a37f4bb4a3ffb1443ef2584b1
Elias Fleckenstein <eliasfleckenstein@web.de>
2020-05-11 13:02:06 +0200

    Updated Ping

--- playerlist.lua
+++ playerlist.lua
@@ -6,6 +6,7 @@ controls.register_on_press(function(player, key)
 		local players = minetest.get_connected_players()
 		for i, p in pairs(players) do
 			local n = p:get_player_name()
+			local ping = math.max(1, math.ceil(4 - minetest.get_player_information(n).avg_rtt * 4))
 			list[#list + 1] = player:hud_add({
 				hud_elem_type = "text",
 				position = {x = 1, y = 0},
@@ -19,7 +20,7 @@ controls.register_on_press(function(player, key)
 				hud_elem_type = "image",
 				position = {x = 1, y = 0},
 				offset = {x = -5, y = (i - 1) * 18},
-				text = "server_ping_" .. math.ceil(4 - minetest.get_player_information(n).avg_rtt / 2 * 4) .. ".png",
+				text = "server_ping_" .. ping .. ".png",
 				alignment = {x = -1, y = 1},
 				scale = {x = 1.5, y = 1.5},
 				number = 0xFFFFFF,
